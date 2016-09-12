b604b09736bea60f18894aceb608df90a3250045
gn0mesort <gn0mesort@megate.ch>
2016-09-12 14:40:22 -0700

    Signed-off-by: gn0mesort <gn0mesort@megate.ch>

--- class/MarkSort/main.cpp
+++ class/MarkSort/main.cpp
@@ -9,11 +9,9 @@
 #include <iostream> //I/O
 #include <cstdlib> //C Standard Library
 #include <ctime> //Time
-//User Libraries
 
 using namespace std;
 
-//Global Constants
 
 //Function Prototypes
 void fillAry(int[], int);
@@ -23,13 +21,12 @@ void prntAry(int[], int, int);
 //Begin Execution
 int main(int argc, char** argv) {
     //Declare Variables
-    const int SIZE = 100, LINES = 10;
-    int array[SIZE]; 
+    const int SIZE = 100, //Size of the array to sort
+              LINES = 10; //Number of values to print per line
+    int array[SIZE]; //The array to be sorted
     
     //Initialize rand()
     srand(static_cast<int>(time(0)));
-    
-    //Input Data
     
     //Fill Array
     fillAry(array, SIZE);
@@ -44,12 +41,31 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+//Fill Array
+//Fill an array with random 2 digit numbers
+//INPUT:
+//a[]
+//An array to fill
+//n
+//The length of the array to fill
+//OUTPUT:
+//a[]
+//The filled array
 void fillAry(int a[], int n){
     for(int i = 0; i < n; ++i){
         a[i] = rand() % 90 + 10; //Generate random 2 digit numbers
     }
 }
 
+//Print Array
+//Print an integer array to standard output
+//INPUT:
+//a[]
+//The array to print
+//n
+//The length of the array
+//perLine
+//The number of values to print per line
 void prntAry(int a[], int n, int perLine){
     for(int i = 0; i < n; ++i){
         cout << a[i] << " ";
@@ -58,6 +74,16 @@ void prntAry(int a[], int n, int perLine){
     cout << endl;
 }
 
+//Mark Sort
+//Sort an array of integers
+//INPUT:
+//a[]
+//The array to sort
+//n
+//The length of the array
+//OUTPUT:
+//a[]
+//The sorted array
 void mrkSort(int a[], int n){
     for(int i = 0; i < n - 1; ++i){
         for(int j = i + 1; j < n; ++j){
--- class/MarkSort_StaticArray_v3_Database/main.cpp
+++ class/MarkSort_StaticArray_v3_Database/main.cpp
@@ -9,11 +9,8 @@
 #include <iostream> //I/O
 #include <cstdlib> //C Standard Library
 #include <ctime> //Time
-//User Libraries
 
 using namespace std;
-
-//Global Constants
 
 //Function Prototypes
 void fillAry(int[], int[], int);
@@ -24,14 +21,14 @@ void prntAry(int[], int[], int, int);
 //Begin Execution
 int main(int argc, char** argv) {
     //Declare Variables
-    const int SIZE = 100, LINES = 10;
-    int utilize = SIZE / 2;
-    int array[SIZE], index[SIZE]; 
+    const int SIZE = 100, //Size of the array to sort  
+              LINES = 10; //Number of values to print per line
+    int utilize = SIZE / 2; //The size of the array to actually fill
+    int array[SIZE], //The array to be sorted
+        index[SIZE]; //An indexing array for the array to be sorted
     
     //Initialize rand()
     srand(static_cast<int>(time(0)));
-    
-    //Input Data
     
     //Fill Array
     fillAry(array, index, utilize);
@@ -55,6 +52,20 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+//Fill Array
+//Fill an array with random 2 digit numbers
+//INPUT:
+//a[]
+//An array to fill
+//index[]
+//An indexing array to fill with the indexes in a[]
+//n
+//The length of the array to fill
+//OUTPUT:
+//a[]
+//The filled array
+//index[]
+//The filled indexing array
 void fillAry(int a[], int index[], int n){
     for(int i = 0; i < n; ++i){
         a[i] = rand() % 90 + 10; //Generate random 2 digit numbers
@@ -62,6 +73,15 @@ void fillAry(int a[], int index[], int n){
     }
 }
 
+//Print Array
+//Print an integer array to standard output
+//INPUT:
+//a[]
+//The array to print
+//n
+//The length of the array
+//perLine
+//The number of values to print per line
 void prntAry(int a[], int n, int perLine){
     for(int i = 0; i < n; ++i){
         cout << a[i] << " ";
@@ -70,6 +90,17 @@ void prntAry(int a[], int n, int perLine){
     cout << endl;
 }
 
+//Print Array
+//Print an integer array to standard output
+//INPUT:
+//a[]
+//The array to print
+//index[]
+//The indexing array for the array to print
+//n
+//The length of the array
+//perLine
+//The number of values to print per line
 void prntAry(int a[], int index[], int n, int perLine){
     for(int i = 0; i < n; ++i){
         cout << a[index[i]] << " ";
@@ -78,6 +109,18 @@ void prntAry(int a[], int index[], int n, int perLine){
     cout << endl;
 }
 
+//Mark Sort
+//Sort an array of integers
+//INPUT:
+//a[]
+//The array containing index[]'s underlying values
+//index[]
+//The indexing array to sort
+//n
+//The length of the array
+//OUTPUT:
+//index[]
+//The sorted indexing array
 void mrkSort(int a[], int index[], int n){
     for(int i = 0; i < n - 1; ++i){
         for(int j = i + 1; j < n; ++j){
