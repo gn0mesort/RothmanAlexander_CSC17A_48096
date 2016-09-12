6cc8482bed2ab35318326ffa73bd127af9bb462a
gn0mesort <gnomesort@megate.ch>
2016-09-12 13:04:32 -0700

    Signed-off-by: gn0mesort <gnomesort@megate.ch>

---
+++ class/CPPMainTemplate/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Alexander Rothman
+ * Created on September 12, 2016, 1:01 PM
+ * CPP Program Main Template
+ */
+
+//System Libraries
+#include <iostream>
+
+//User Libraries
+
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Begin Execution
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process Data
+    
+    //Display Output
+    
+    //Exit
+    return 0;
+}
+
