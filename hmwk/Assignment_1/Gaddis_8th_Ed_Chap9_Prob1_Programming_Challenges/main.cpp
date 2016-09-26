b73c924d33dc7826207947ecbe3c8937e286f275
gn0mesort <gnomesort@megate.ch>
2016-09-26 09:06:10 -0700

    Signed-off-by: gn0mesort <gnomesort@megate.ch>

---
+++ hmwk/Assignment_1/Gaddis_8th_Ed_Chap9_Prob1_Programming_Challenges/main.cpp
@@ -0,0 +1,49 @@
+/*
+ * File:   main.cpp
+ * Author: Alexander Rothman <arothman@student.rcc.edu>
+ * Purpose: Generate a dynamic array of a variable size.
+ * Array is initialized to 0.
+ * Created on September 26, 2016
+ */
+
+#include <iostream>
+using namespace std;
+
+int* genArr(int);
+
+int main(int argc, char** argv){
+    int length = 0;
+    int *array = NULL;
+    cout << "How many elements should be allocated: ";
+    cin >> length;
+
+    array = genArr(length);
+    cout << "ARRAY POINTER: " << array << endl;
+    cout << "ARRAY CONTENTS:" << endl;
+    for(int i = 0; i < length; ++i){
+        cout << array[i];
+        if(length > 10){
+            if((i + 1) % 10 == 0){
+                cout << endl;
+            }
+            else{
+                cout << ", ";
+            }
+        }
+        else{
+            if((i + 1) < length){
+                cout << ", ";
+            }
+        }
+    }
+
+    delete [] array;
+
+    return 0;
+}
+
+int* genArr(int length){
+    return new int[length]{
+    };
+}
+
---
+++ hmwk/Assignment_1/Gaddis_8th_Ed_Chap9_Prob5_Programming_Challenges/main.cpp
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: Alexander Rothman <arothman@student.rcc.edu>
+ * Purpose: Preform a computation using pointers instead of references
+ * Created on September 26, 2016
+ */
+
+#include <iostream>
+using namespace std;
+
+
+int compute(int*, int*);
+
+int main(int argc, char** argv){
+    int xVal = 0,
+            yVal = 0;
+    int *x = &xVal,
+            *y = &yVal;
+
+
+    cout << "ENTER A VALUE FOR X: ";
+    cin >> xVal;
+    cout << "ENTER A VALUE FOR Y: ";
+    cin >> yVal;
+    cout << "X: " << *x << endl;
+    cout << "Y: " << *y << endl;
+    cout << "compute(X, Y): " << compute(x, y) << endl;
+    cout << "X: " << *x << endl;
+    cout << "Y: " << *y << endl;
+
+    return 0;
+}
+
+int compute(int *x, int *y){
+    int temp = *x;
+    *x = *y * 10;
+    *y = temp * 10;
+    return *x + *y;
+}
+
